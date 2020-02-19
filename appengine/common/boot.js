ace8406dbe9663e9e2f5693bf1eb229233f63466
Kiran Nayak <niekiran@gmail.com>
2020-02-19 17:10:56 +0530

    json file for kannada and build files modifications

--- appengine/common/boot.js
+++ appengine/common/boot.js
@@ -23,12 +23,7 @@ if (location.host == 'blockly-games.appspot.com') {
 
   // Supported languages (consistent across all apps).
   window['BlocklyGamesLanguages'] = [
-    'ar', 'be', 'be-tarask', 'bg', 'bn', 'br', 'cs', 'da', 'de', 'el',
-    'en', 'eo', 'es', 'eu', 'fa', 'fi', 'fo', 'fr', 'gl', 'ha', 'he',
-    'hi', 'hu', 'hy', 'ia', 'id', 'ig', 'is', 'it', 'ja', 'kab', 'ko',
-    'lt', 'lv', 'ms', 'my', 'nb', 'nl', 'pl', 'pms', 'pt', 'pt-br',
-    'ro', 'ru', 'sc', 'sk', 'sl', 'sq', 'sr', 'sv', 'th', 'ti', 'tr',
-    'uk', 'ur', 'vi', 'yo', 'zh-hans', 'zh-hant'
+	  'en','kn'
   ];
 
   // Use a series of heuristics that determine the likely language of this user.
